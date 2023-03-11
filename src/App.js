f061c807bd28d2cce1cc89868b0d253a6933af7e
adam <adammarey51@gmail.com>
2023-03-10 22:27:55 -0700

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Home from './pages/Home';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
+      <Footer />
     </div>
   );
 }
